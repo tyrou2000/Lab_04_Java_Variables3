64ad13322f82570ed471f75b5521c0619f2cf494
Ty <tyrou2000@gmail.com>
2023-09-14 15:21:40 -0400

    initial commit 1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo (Remainder)
+        int intOperandA = 4;
+        int intOperandB = 7;
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The Sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + "is " + intDifference);
+
+        // rest of int statements
+
+        double doubleOperandA = 3.14;
+        double doubleOperandB = 3.40;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The Sum of " + doubleOperandA + " and " + doubleOperandB + " is " + intSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + intProduct);
+
+        doubleDifference = doubleOperandA + doubleOperandB;
+
+
+    }
+}
